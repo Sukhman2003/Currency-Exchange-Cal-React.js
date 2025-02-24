004fb0c67e5752b0d9884ad0766014e84f38e35f
Sukhman2003 <112985333+Sukhman2003@users.noreply.github.com>
2025-02-25 00:25:06 +0530

    little commit

--- src/assets/Components/InputBox.jsx
+++ src/assets/Components/InputBox.jsx
@@ -15,7 +15,7 @@ function InputBox({
    
 
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+        <div className={`bg-white p-3 text-black rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
                 <label  className="text-black/40 mb-2 inline-block">
                     {label}
